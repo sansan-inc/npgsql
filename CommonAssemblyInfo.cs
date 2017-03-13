21cb39d3bacd2dda8422f8d5b71bf257153afc6a
ryoji-kawamura <r.kawamura@sansan.com>
2017-03-13 19:00:50 +0900

    Bump version to 2.2.7.1.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Resources;
 [assembly: AssemblyProductAttribute("Npgsql")]
 [assembly: AssemblyCopyrightAttribute("Copyright © 2002 - 2014 Npgsql Development Team")]
 [assembly: AssemblyTrademarkAttribute("")]
-[assembly: AssemblyVersionAttribute("2.2.7")]
-[assembly: AssemblyFileVersionAttribute("2.2.7")]
-[assembly: AssemblyInformationalVersionAttribute("2.2.7")]
+[assembly: AssemblyVersionAttribute("2.2.7.1")]
+[assembly: AssemblyFileVersionAttribute("2.2.7.1")]
+[assembly: AssemblyInformationalVersionAttribute("2.2.7.1")]
 [assembly: NeutralResourcesLanguageAttribute("en", UltimateResourceFallbackLocation.MainAssembly)]
 
